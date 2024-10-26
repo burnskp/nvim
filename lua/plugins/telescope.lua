20b162f1f895ba8cfc13716883849396b6b0abaf
Kevin Burns <kevin@burnskp.com>
2024-10-25 22:32:22 -0500

    Add Telescope frecency

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,22 @@
-local Util = require("lazyvim.util")
 return {
-	"telescope.nvim",
-	opts = {
-		defaults = {
-			layout_strategy = "vertical",
-			layout_config = {
-				preview_cutoff = 10,
+	{
+		"telescope.nvim",
+		opts = {
+			defaults = {
+				layout_strategy = "vertical",
+				layout_config = {
+					preview_cutoff = 10,
+				},
 			},
+		},
+	},
+	{
+		"nvim-telescope/telescope-frecency.nvim",
+		config = function()
+			require("telescope").load_extension("frecency")
+		end,
+		keys = {
+			{ "<leader>sf", "<cmd>Telescope frecency<cr>", desc = "Frecuency" },
 		},
 	},
 }
